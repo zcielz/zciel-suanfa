b2a9f9141cda21ad4feab9d08bf43501e80169b7
zcielz <zciel_plus@outlook.com>
2019-07-03 10:29:02 +0800

    7.03

--- src/main/java/cn/zciel/class02/Code_13_CopyListWithRandom.java
+++ src/main/java/cn/zciel/class02/Code_13_CopyListWithRandom.java
@@ -1,6 +1,7 @@
 package cn.zciel.class02;
 
-import lombok.extern.slf4j.Slf4j;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @author YINZHEN
@@ -18,5 +19,114 @@ public class Code_13_CopyListWithRandom {
             this.value = data;
         }
     }
+
+    //建一个map集合，将每个节点放进去，然后遍历
+    public static Node copyListWithRand1(Node head) {
+        Map<Node, Node> nodeMap = new HashMap();
+        Node cur = head;
+        while (cur != null) {
+            nodeMap.put(cur, new Node(cur.value));
+            cur = cur.next;
+        }
+        cur = head;
+        while (cur != null) {
+            nodeMap.get(cur).rand = cur.rand;
+            nodeMap.get(cur).next = nodeMap.get(cur.next);
+            cur = cur.next;
+        }
+        return nodeMap.get(head);
+    }
+
+    //在每个节点后面再加一个节点
+    public static Node copyListWithRand2(Node head) {
+        if (head == null) {
+            return null;
+        }
+        Node cur = head;
+        Node next = null;
+        //copy node and link to every node
+        while (cur != null) {
+            next = cur.next;
+            cur.next = new Node(cur.value);
+            cur.next.next = next;
+            cur = next;
+        }
+        cur = head;
+        Node curCopy = null;
+        //set copy node rand
+        while (cur != null) {
+            next = cur.next.next;
+            curCopy = cur.next;
+            curCopy.rand = cur.rand != null ? cur.rand.next : null;
+            cur = next;
+        }
+        Node res = head.next;
+        cur = head;
+        //split
+        while (cur != null) {
+            next = cur.next.next;
+            curCopy = cur.next;
+            cur.next = next;
+            curCopy.next = next == null ? null : next.next;
+            cur = next;
+        }
+        return res;
+    }
+
+    public static void printRandLinkedList(Node head) {
+        Node cur = head;
+        System.out.println("order: ");
+        while (cur != null) {
+            System.out.print(cur.value + " ");
+            cur = cur.next;
+        }
+        System.out.println();
+        cur = head;
+        System.out.println("rand: ");
+        while (cur != null) {
+            System.out.print(cur.rand == null ? "- " : cur.rand.value + " ");
+            cur = cur.next;
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        Node head = null;
+        Node res1 = null;
+        Node res2 = null;
+        printRandLinkedList(head);
+        res1 = copyListWithRand1(head);
+        printRandLinkedList(res1);
+        res2 = copyListWithRand2(head);
+        printRandLinkedList(res2);
+        printRandLinkedList(head);
+        System.out.println("=========================");
+
+        head = new Node(1);
+        head.next = new Node(2);
+        head.next.next = new Node(3);
+        head.next.next.next = new Node(4);
+        head.next.next.next.next = new Node(5);
+        head.next.next.next.next.next = new Node(6);
+
+        head.rand = head.next.next.next.next.next; // 1 -> 6
+        head.next.rand = head.next.next.next.next.next; // 2 -> 6
+        head.next.next.rand = head.next.next.next.next; // 3 -> 5
+        head.next.next.next.rand = head.next.next; // 4 -> 3
+        head.next.next.next.next.rand = null; // 5 -> null
+        head.next.next.next.next.next.rand = head.next.next.next; // 6 -> 4
+
+        printRandLinkedList(head);
+        res1 = copyListWithRand1(head);
+        System.out.println("res1:");
+        printRandLinkedList(res1);
+        res2 = copyListWithRand2(head);
+        System.out.println("res2:");
+        printRandLinkedList(res2);
+        printRandLinkedList(head);
+        System.out.println("=========================");
+
+    }
+
 
 }
